26d41e7b008fc548605ab0237acd0ea7aecfdbda
Ryan Butalid <ryanbutalid@Ryans-MacBook-Air.local>
2025-08-03 22:29:41 +0800

    hallo

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,12 +1,8 @@
-
 import { UserAuth } from "./context/AuthContext";
 import LandingPage from "./components/visitor/LandingPage";
 import Header from "./components/visitor/Header";
 import Footer from "./components/visitor/Footer";
-
-
-
-
+import Navigation from "./components/registered/Navigation";
 
 function Landing() {
   const { session } = UserAuth();
